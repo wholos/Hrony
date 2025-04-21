98f519cb6002bdb336d32e4d0a7d4525ed979bcb
holos <holosnikitatt@gmail.com>
2025-04-21 10:44:36 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,135 +1,157 @@
-/*Package Manager, 2000-2025 Holos*/
+// *Nikita Shyshatskyi (Holos)* //
+// *RePKG* //
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <curl/curl.h>
-#include <json-c/json.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-#define MAX_URL_LENGTH 1024
+#define REPO_URL ""
+#define REPO_PATH ""
+#define INSTALL_DIR "/usr/bin/"
 
-struct file {
-    char name[128];
-    char url[MAX_URL_LENGTH];
-};
+// HTTP client
+void download_file(const char *host, const char *path, const char *output_file) {
+    int sock;
+    struct sockaddr_in server_addr;
+    struct hostent *he;
 
-struct repository {
-    char name[128];
-    char url[MAX_URL_LENGTH];
-    struct file files[10];
-    int file_count;
-};
-
-struct repository repos[10];
-int repo_count = 0;
-
-void load_config(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("Error in opening repository config!");
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Error!");
         exit(1);
     }
 
-    char buffer[4096];
-    size_t len = fread(buffer, 1, sizeof(buffer), file);
-    fclose(file);
+    if ((he = gethostbyname(host)) == NULL) {
+        perror("Error!");
+        exit(1);
+    }
 
-    struct json_object *parsed_json = json_tokener_parse(buffer);
-    struct json_object *repos_json;
-    json_object_object_get_ex(parsed_json, "repos", &repos_json);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(80);
+    memcpy(&server_addr.sin_addr, he->h_addr_list[0], he->h_length);
 
-    repo_count = json_object_array_length(repos_json);
-    for (int i = 0; i < repo_count; i++) {
-        struct json_object *repo_json = json_object_array_get_idx(repos_json, i);
-        struct json_object *name_json = json_object_object_get(repo_json, "name");
-        struct json_object *url_json = json_object_object_get(repo_json, "url");
-        struct json_object *files_json = json_object_object_get(repo_json, "files");
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error!");
+        exit(1);
+    }
 
-        strcpy(repos[i].name, json_object_get_string(name_json));
-        strcpy(repos[i].url, json_object_get_string(url_json));
+    char request[512];
+    snprintf(request, sizeof(request),
+        "GET %s HTTP/1.1\r\n"
+        "Host: %s\r\n"
+        "Connection: close\r\n\r\n",
+        path, host);
 
-        repos[i].file_count = json_object_array_length(files_json);
-        for (int j = 0; j < repos[i].file_count; j++) {
-            struct json_object *file_json = json_object_array_get_idx(files_json, j);
-            struct json_object *file_name_json = json_object_object_get(file_json, "name");
-            struct json_object *file_url_json = json_object_object_get(file_json, "url");
+    if (send(sock, request, strlen(request), 0) < 0) {
+        perror("Error!");
+        close(sock);
+        exit(1);
+    }
 
-            strcpy(repos[i].files[j].name, json_object_get_string(file_name_json));
-            strcpy(repos[i].files[j].url, json_object_get_string(file_url_json));
+    FILE *output = fopen(output_file, "wb");
+    if (!output) {
+        perror("Error!");
+        close(sock);
+        exit(1);
+    }
+
+    char buffer[1024];
+    int headers_ended = 0;
+    ssize_t bytes_read;
+
+    while ((bytes_read = recv(sock, buffer, sizeof(buffer), 0)) > 0) {
+        if (!headers_ended) {
+            char *header_end = strstr(buffer, "\r\n\r\n");
+            if (header_end) {
+                headers_ended = 1;
+                fwrite(header_end + 4, 1, bytes_read - (header_end - buffer) - 4, output);
+            }
+        } else {
+            fwrite(buffer, 1, bytes_read, output);
         }
     }
 
-    json_object_put(parsed_json);
+    fclose(output);
+    close(sock);
 }
 
-size_t write_callback(void *ptr, size_t size, size_t nmemb, FILE *stream) {
-    return fwrite(ptr, size, nmemb, stream);
+// Install packages
+void install_pkg(const char *pkg_name) {
+    printf("Install %s...\n", pkg_name);
+
+    char url_path[256];
+    snprintf(url_path, sizeof(url_path), "%s%s", REPO_PATH, pkg_name);
+
+    char tmp_path[256];
+    snprintf(tmp_path, sizeof(tmp_path), "/tmp/%s", pkg_name);
+
+    download_file(REPO_URL, url_path, tmp_path);
+
+    chmod(tmp_path, 0755);
+
+    char install_path[256];
+    snprintf(install_path, sizeof(install_path), "%s%s", INSTALL_DIR, pkg_name);
+
+    char move_cmd[512];
+    snprintf(move_cmd, sizeof(move_cmd), "mv %s %s", tmp_path, install_path);
+    system(move_cmd);
+
+    printf("%s installed %s\n", pkg_name, INSTALL_DIR);
 }
 
-int download_package(const char *url, const char *output_filename) {
-    CURL *curl;
-    CURLcode res;
+// Remove packages
+void remove_pkg(const char *pkg_name) {
+    printf("Remove %s...\n", pkg_name);
 
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
+    char path[256];
+    snprintf(path, sizeof(path), "%s%s", INSTALL_DIR, pkg_name);
 
-    if (curl) {
-        FILE *fp = fopen(output_filename, "wb");
-        if (!fp) {
-            perror("Error in opening file to write!");
-            return -1;
-        }
-
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-
-        res = curl_easy_perform(curl);
-
-        if (res != CURLE_OK) {
-            fprintf(stderr, "Error in download: %s\n", curl_easy_strerror(res));
-            fclose(fp);
-            curl_easy_cleanup(curl);
-            curl_global_cleanup();
-            return -1;
-        }
-
-        fclose(fp);
-        curl_easy_cleanup(curl);
-        curl_global_cleanup();
+    if (access(path, F_OK) == 0) {
+        char cmd[256];
+        snprintf(cmd, sizeof(cmd), "rm -f %s", path);
+        system(cmd);
+        printf("%s removed.\n", pkg_name);
+    } else {
+        printf("Error: %s not installed.\n", pkg_name);
     }
-    return 0;
 }
 
+// Update repos
+void update_repo() {
+    printf("Update repos...\n");
+    printf("Repos updated.\n");
+}
+
+// Help
+void print_help() {
+    printf("Use:\n");
+    printf("  repkg -install <PKG>  - Install package\n");
+    printf("  repkg -remove <PKG>   - Remove package\n");
+    printf("  repkg -update         - Update repos\n");
+}
+
+// Main function
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s [PACKAGE]\n", argv[0]);
-        return 1;
-    }
-
-    const char *package_name = argv[1];
-
-    load_config("repo.json");
-
-    int found = 0;
-    for (int i = 0; i < repo_count; i++) {
-        for (int j = 0; j < repos[i].file_count; j++) {
-            if (strcmp(repos[i].files[j].name, package_name) == 0) {
-                printf("Download file: %s\n", repos[i].files[j].name);
-                if (download_package(repos[i].files[j].url, repos[i].files[j].name) != 0) {
-                    return 1;
-                }
-                found = 1;
-                break;
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') {
+            switch (argv[i][1]) {
+                case 'install': // Install flag
+			        install_pkg(argv[2]); return 0;
+                case 'remove': // Remove flag
+			        remove_pkg(argv[2]); return 0;
+		        case 'update': // Update flag
+		            update_repo(); return 0;
+		        case 'help': // Help Flag
+			        print_help(); return 0;
+		        default:  printf("No flag: '%s'\n", argv[i]); return 0;
             }
         }
-        if (found) break;
     }
-
-    if (!found) {
-        fprintf(stderr, "Package %s no found in repositories.\n", package_name);
-        return 1;
-    }
-
-    printf("Package %s download.\n", package_name);
+    printf("Use repkg -help.");
     return 0;
 }
