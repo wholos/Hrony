cea676abacee9ac58eea611cf30b2c0b3e908eb2
holos <holosnikitatt@gmail.com>
2025-07-17 14:38:02 +0300

    Update main.nim

--- src/main.nim
+++ src/main.nim
@@ -1,1 +1,19 @@
-...
+import os, osproc, strutils
+
+for arg in commandLineParams():
+  case arg
+    of "--help":
+      echo "Install package - install"
+      echo "Remove package - delete"
+      quit(0)
+    of "install":
+      echo "Install..."
+      quit(0)
+    of "delete":
+      echo "Delete..."
+      quit(0)
+    else:
+      echo "Unknown argument: ", arg
+      quit(0)
+
+echo "Enter argument, arguments in --help."
